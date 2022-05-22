c30a27cd71d67ca1dba0aec850a623a25fa30cf8
bun133 <bun133.work@gmail.com>
2022-05-22 21:39:55 +0900

    Fix:CharDrawing

--- main/src/main/java/net/numalab/mojii/lang/FontDrawableChar.kt
+++ main/src/main/java/net/numalab/mojii/lang/FontDrawableChar.kt
@@ -1,6 +1,8 @@
 package net.numalab.mojii.lang
 
+import java.awt.Color
 import java.awt.Font
+import java.awt.FontMetrics
 import java.awt.image.BufferedImage
 
 /**
@@ -10,7 +12,12 @@ class FontDrawableChar(char: Char, val font: Font) : DrawableChar(char) {
     override fun draw(img: BufferedImage): BufferedImage {
         val g = img.graphics
         g.font = font
-        g.drawString(char.toString(), 0, 0)
+        val metrics = g.fontMetrics
+        val s = "" + char
+        val x = 64 - metrics.stringWidth(s) / 2
+        val y = (128 - metrics.height) / 2 + metrics.ascent
+        g.color = Color.BLACK
+        g.drawString(s, x, y)
         return img
     }
 }
