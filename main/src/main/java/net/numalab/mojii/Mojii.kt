ca30cf0c244826409e92636dd25a45b112288c9a
bun133 <bun133.work@gmail.com>
2022-05-26 08:37:08 +0900

    Add:ScoreBoard

--- main/src/main/java/net/numalab/mojii/Mojii.kt
+++ main/src/main/java/net/numalab/mojii/Mojii.kt
@@ -4,10 +4,12 @@ import dev.kotx.flylib.flyLib
 import net.numalab.mojii.command.MojiiCommand
 import net.numalab.mojii.game.MojiiGame
 import net.numalab.mojii.listener.Listeners
+import net.numalab.mojii.scoreboard.MojiiGameScoreBoard
 import org.bukkit.plugin.java.JavaPlugin
 
 class Mojii : JavaPlugin() {
     lateinit var config: MojiiConfig
+    lateinit var scoreBoard: MojiiGameScoreBoard
 
     init {
         flyLib {
@@ -20,6 +22,7 @@ class Mojii : JavaPlugin() {
             it.saveConfigIfAbsent()
             it.loadConfig()
         }
+        scoreBoard = MojiiGameScoreBoard()
         Listeners(this)
     }
 
---
+++ main/src/main/java/net/numalab/mojii/scoreboard/MojiiGameScoreBoard.kt
@@ -0,0 +1,31 @@
+package net.numalab.mojii.scoreboard
+
+import com.github.bun133.bukkitfly.component.text
+import org.bukkit.Bukkit
+import org.bukkit.scoreboard.Objective
+import org.bukkit.scoreboard.Team
+
+
+class MojiiGameScoreBoard() {
+    companion object {
+        private fun getObjective(): Objective {
+            return Bukkit.getServer().scoreboardManager.mainScoreboard.getObjective("MojiiGame")
+                ?: Bukkit.getServer().scoreboardManager.mainScoreboard.registerNewObjective(
+                    "MojiiGame",
+                    "dummy",
+                    text("スコア")
+                )
+        }
+    }
+
+    private val obj = getObjective()
+
+    val score = mutableMapOf<Team, Int>()
+
+    fun update() {
+        score.forEach { (t, u) ->
+            val score = obj.getScore(t.name)
+            score.score = u
+        }
+    }
+}
