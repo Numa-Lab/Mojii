d0c1675af0f1441be55fd9a7de347fdefd6b9ceb
bun133 <bun133.work@gmail.com>
2022-06-06 18:40:30 +0900

    Fix:前方一致に対応

--- main/src/main/java/net/numalab/mojii/judge/StringGetter.kt
+++ main/src/main/java/net/numalab/mojii/judge/StringGetter.kt
@@ -6,10 +6,26 @@ import org.bukkit.entity.ItemFrame
 
 class StringGetter {
     companion object {
-        // FIXME 部分一致に対応していない
         fun getFrom(from: Location): List<Pair<String, List<Pair<MojiiMap, ItemFrame>>>> {
-            val maps = MojiiMapGetter.getFrom(from)
+            val maps = MojiiMapGetter.getFrom(from).map { it.getArr() }.flatten()
             return maps.map { l -> l.map { it.first.char.char }.joinToString(separator = "") to l }
         }
     }
+}
+
+/**
+ * 先頭から一つずつ末尾のインデックスをずらしていったListを返す
+ */
+private fun <T> List<T>.getArr(): List<List<T>> {
+    return indices.map {
+        this.copy(0..it)
+    }
+}
+
+private fun <T> List<T>.copy(intRange: IntRange): List<T> {
+    val list = mutableListOf<T>()
+    for (i in intRange) {
+        list.add(this[i])
+    }
+    return list
 }
