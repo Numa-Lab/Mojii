f85ca9c137a55c517437058de723eefe39386894
bun133 <bun133.work@gmail.com>
2022-06-08 06:31:13 +0900

    Add:著作権表示

--- main/src/main/java/net/numalab/mojii/command/MojiiStartCommand.kt
+++ main/src/main/java/net/numalab/mojii/command/MojiiStartCommand.kt
@@ -1,10 +1,15 @@
 package net.numalab.mojii.command
 
+import com.github.bun133.bukkitfly.component.plus
+import com.github.bun133.bukkitfly.component.text
 import dev.kotx.flylib.command.Command
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.numalab.mojii.Mojii
 import net.numalab.mojii.game.MojiiGame
 import net.numalab.mojii.game.MojiiGameSetting
 import net.numalab.mojii.lang.Lang
+import org.bukkit.Bukkit
 
 class MojiiStartCommand : Command("start") {
     init {
@@ -28,11 +33,20 @@ class MojiiStartCommand : Command("start") {
                 }
 
                 val tm = pl.config.team().toList()
-                if (tm.isEmpty()){
+                if (tm.isEmpty()) {
                     fail("チームが設定されていません")
-                }else{
+                } else {
                     pl.currentGame = MojiiGame(MojiiGameSetting(pl.config.team().toList(), lang, clearCardAmount))
-                    success("ゲームを開始しました")
+                    Bukkit.broadcast(text("ゲームを開始しました",NamedTextColor.GREEN))
+
+                    // 著作権上の表示
+                    Bukkit.broadcast(
+                        text("【著作権上必要な表示】") + Component.newline() +
+                                text(
+                                    "このプラグインはCC BY-SA 3.0(https://creativecommons.org/licenses/by-sa/3.0/)で二次利用許諾されたWikipedia(https://www.wikipedia.org/)のコピーを使用しています。",
+                                    NamedTextColor.RED
+                                )
+                    )
                 }
             }
         }
